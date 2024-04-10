2a4d5d29fecef0793e4f45c71e63da1e142c62fb
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-04-10 11:02:30 +0200

    Add project files.

---
+++ Klasser uppgift 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Klasser_uppgift_2;
+using System;
+
+namespace Uppgift2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Namn namn2 = new("Ernst", "Osbeck", new DateTime(2006, 03, 10), 75, 185);
+            Console.WriteLine("Namn: " + namn2.SetNamn);
+            Console.WriteLine("Födelsedag: " + namn2.setDatum);
+            Console.WriteLine("Myndig: " + namn2.KontrolleraDatum());
+            Console.WriteLine("BMI: " + namn2.BMI());
+        }
+    }
+}
